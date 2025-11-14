4c912d7452da0ddd63021c5005d42d2d35e37605
zakriakhanx <zakriakhanz2003@gmail.com>
2025-11-14 13:22:45 -0500

    Added login, logout and signup functionality

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,6 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+        path('register/', TemplateView.as_view(template_name="index.html")),
+        path('login/', TemplateView.as_view(template_name="index.html")),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
         path('contact/', TemplateView.as_view(template_name="Contact.html")),
