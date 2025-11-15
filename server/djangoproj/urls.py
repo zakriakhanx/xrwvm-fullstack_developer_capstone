06d53acafaea87d0e743d3e4b95fb522f126b340
zakriakhanx <zakriakhanz2003@gmail.com>
2025-11-15 13:57:18 -0500

    created a dealer list, dealer details, and a provision for adding reviews

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,10 +21,13 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path('register/', TemplateView.as_view(template_name="index.html")),
-        path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
     path('djangoapp/', include('djangoapp.urls')),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
-        path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
